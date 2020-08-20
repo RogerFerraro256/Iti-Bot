6711eae1e96ec07c4dae27b1c31fa9f11769f2aa
root <root@Wiki.manual.atranet.com.br>
2020-08-20 17:03:13 -0300

    ultima vez

--- index.js
+++ index.js
@@ -27,18 +27,49 @@ client.on('message', async message =>{
 
  if (message.content.startsWith(`${prefix}play`)) {
      execute(message, serverQueue);
+	message.channel.send("foi");
      return;
  } else if (message.content.startsWith(`${prefix}skip`)) {
      skip(message, serverQueue);
+	message.channel.send("foi");
      return;
  } else if (message.content.startsWith(`${prefix}stop`)) {
      stop(message, serverQueue);
+	message.channel.send("foi");
      return;
  } else {
      message.channel.send("You need to enter a valid command!");
  }
+
+const queue = new Map();
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+}
+
+const songInfo = await ytdl.getInfo(args[1]);
+const song = {
+ title: songInfo.title,
+ url: songInfo.video_url,
+};
+
+
+
+
 //41
 //42
-}
+})
 //43
 //44
