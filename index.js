f443e3ac71d85cd53903ad10a096ab2407bb5fd7
Rogério Salvadeo Vieira <rogerferraro256@gmail.com>
2020-08-20 16:51:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,23 +20,23 @@ client.once('disconnect', () => {
 
 client.on('message', async message =>{
 
-if (message.author.bot) return;
-if (!message.content.startsWith(prefix)) return;
+ if (message.author.bot) return;
+ if (!message.content.startsWith(prefix)) return;
 
-const serverQueue = queue.get(message.guild.id);
+ const serverQueue = queue.get(message.guild.id);
 
-if (message.content.startsWith(`${prefix}play`)) {
-    execute(message, serverQueue);
-    return;
-} else if (message.content.startsWith(`${prefix}skip`)) {
-    skip(message, serverQueue);
-    return;
-} else if (message.content.startsWith(`${prefix}stop`)) {
-    stop(message, serverQueue);
-    return;
-} else {
-    message.channel.send("You need to enter a valid command!");
-}
+ if (message.content.startsWith(`${prefix}play`)) {
+     execute(message, serverQueue);
+     return;
+ } else if (message.content.startsWith(`${prefix}skip`)) {
+     skip(message, serverQueue);
+     return;
+ } else if (message.content.startsWith(`${prefix}stop`)) {
+     stop(message, serverQueue);
+     return;
+ } else {
+     message.channel.send("You need to enter a valid command!");
+ }
 //41
 //42
 }
