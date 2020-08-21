d9e69cf683cab3ac69eb9847864a1315fc2ec78f
roger <rogerferraro256@gmail.com>
2020-08-21 14:12:10 -0300

    added new features and corrected some bugs

--- index.js
+++ index.js
@@ -20,56 +20,22 @@ client.once('disconnect', () => {
 
 client.on('message', async message =>{
 
- if (message.author.bot) return;
- if (!message.content.startsWith(prefix)) return;
-
- const serverQueue = queue.get(message.guild.id);
-
- if (message.content.startsWith(`${prefix}play`)) {
-     execute(message, serverQueue);
-	message.channel.send("foi");
-     return;
- } else if (message.content.startsWith(`${prefix}skip`)) {
-     skip(message, serverQueue);
-	message.channel.send("foi");
-     return;
- } else if (message.content.startsWith(`${prefix}stop`)) {
-     stop(message, serverQueue);
-	message.channel.send("foi");
-     return;
- } else {
-     message.channel.send("You need to enter a valid command!");
+ if (message.author.bot){
+   console.log(message);
+ };
+ if (!message.content.startsWith(prefix)){
+  console.log(message);
+ };
+ 
+ if(message.author.id=='670815053926367249'){
+  message.channel.send("Gi amo você");
  }
+ if (message.content.startsWith(prefix)){
+   message.channel.send("prefixo usado");
+   console.log(message);
+ };
+ 
 
-const queue = new Map();
-
-async function execute(message, serverQueue) {
-  const args = message.content.split(" ");
-
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-    return message.channel.send(
-      "You need to be in a voice channel to play music!"
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return message.channel.send(
-      "I need the permissions to join and speak in your voice channel!"
-    );
-  }
-}
-
-const songInfo = await ytdl.getInfo(args[1]);
-const song = {
- title: songInfo.title,
- url: songInfo.video_url,
-};
-
-
-
-
-//41
-//42
 })
 //43
 //44
